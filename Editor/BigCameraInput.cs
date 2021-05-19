52ad6979010a8718996b029ce7c0f17440fa9517
Matthew Miner <matthew@matthewminer.com>
2021-05-18 22:11:59 -0700

    Save textures for divided projection matrices

--- Editor/BigCameraInput.cs
+++ Editor/BigCameraInput.cs
@@ -9,33 +9,103 @@ namespace UnityEditor.BigImageRecorder
     /// </summary>
     class BigCameraInput : RecorderInput
     {
-        BigCameraInputSettings InputSettings => settings as BigCameraInputSettings;
+        public BigCameraInputSettings InputSettings => settings as BigCameraInputSettings;
+        public RenderTexture[,] OutputRenderTextures { get; private set; }
+
+        Matrix4x4[,] projectionMatrices;
 
         protected override void BeginRecording(RecordingSession session)
         {
             base.BeginRecording(session);
-            // TODO: create render textures
+            OutputRenderTextures = CreateOutputRenderTextures(InputSettings);
+            projectionMatrices = CreateProjectionMatrices(InputSettings);
         }
 
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
-            // TODO: throw out render textures
+
+            foreach (var renderTexture in OutputRenderTextures)
+            {
+                renderTexture.Release();
+            }
+
+            OutputRenderTextures = null;
         }
 
         protected override void NewFrameReady(RecordingSession session)
         {
             base.NewFrameReady(session);
             var camera = GetTargetCamera(InputSettings.CameraTag);
+            var originalTargetTexture = camera.targetTexture;
 
-            // TODO:
-            // save current projection matrix
-            // save current render target
-            // change projection matrix
-            // change render target
-            // render camera
-            // reset render target
-            // reset projection matrix
+            for (var i = 0; i < InputSettings.HorizontalTileCount; i++)
+            {
+                for (var j = 0; j < InputSettings.VerticalTileCount; j++)
+                {
+                    camera.projectionMatrix = projectionMatrices[i, j];
+                    camera.targetTexture = OutputRenderTextures[i, j];
+                    camera.Render();
+                }
+            }
+
+            camera.targetTexture = originalTargetTexture;
+            camera.ResetProjectionMatrix();
+        }
+
+        static RenderTexture[,] CreateOutputRenderTextures(BigCameraInputSettings inputSettings)
+        {
+            var outputRenderTextures = new RenderTexture[inputSettings.HorizontalTileCount, inputSettings.VerticalTileCount];
+            var width = inputSettings.OutputWidth / inputSettings.HorizontalTileCount;
+            var height = inputSettings.OutputHeight / inputSettings.VerticalTileCount;
+
+            for (var i = 0; i < inputSettings.HorizontalTileCount; i++)
+            {
+                for (var j = 0; j < inputSettings.VerticalTileCount; j++)
+                {
+                    var renderTexture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+                    renderTexture.Create();
+                    outputRenderTextures[i, j] = renderTexture;
+                }
+            }
+
+            return outputRenderTextures;
+        }
+
+        static Matrix4x4[,] CreateProjectionMatrices(BigCameraInputSettings inputSettings)
+        {
+            var projectionMatrices = new Matrix4x4[inputSettings.HorizontalTileCount, inputSettings.VerticalTileCount];
+            var camera = GetTargetCamera(inputSettings.CameraTag);
+
+            var top = camera.nearClipPlane * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            var bottom = -top;
+            var left = bottom * camera.aspect;
+            var right = top * camera.aspect;
+
+            // How much of the final image each tile accounts for.
+            var horizontalTilePercent = 1f / inputSettings.HorizontalTileCount;
+            var verticalTilePercent = 1f / inputSettings.VerticalTileCount;
+
+            for (var i = 0; i < inputSettings.HorizontalTileCount; i++)
+            {
+                var tileLeft = left * (1 - 2 * horizontalTilePercent * i);
+                var tileRight = right * (-1 + 2 * horizontalTilePercent * (i + 1));
+
+                for (var j = 0; j < inputSettings.VerticalTileCount; j++)
+                {
+                    var tileTop = top * (1 - 2 * verticalTilePercent * j);
+                    var tileBottom = bottom * (-1 + 2 * verticalTilePercent * (j + 1));
+
+                    var projectionMatrix = camera.projectionMatrix;
+                    projectionMatrix.m00 = 2 * camera.nearClipPlane / (tileRight - tileLeft);
+                    projectionMatrix.m02 = (tileRight + tileLeft) / (tileRight - tileLeft);
+                    projectionMatrix.m11 = 2 * camera.nearClipPlane / (tileTop - tileBottom);
+                    projectionMatrix.m12 = (tileTop + tileBottom) / (tileTop - tileBottom);
+                    projectionMatrices[i, j] = projectionMatrix;
+                }
+            }
+
+            return projectionMatrices;
         }
 
         static Camera GetTargetCamera(string cameraTag)
--- Editor/BigImageRecorder.cs
+++ Editor/BigImageRecorder.cs
@@ -1,4 +1,6 @@
+using System.IO;
 using UnityEditor.Recorder;
+using UnityEngine;
 
 namespace UnityEditor.BigImageRecorder
 {
@@ -11,7 +13,34 @@ namespace UnityEditor.BigImageRecorder
         {
             var path = Settings.FileNameGenerator.BuildAbsolutePath(session);
             var input = m_Inputs[0] as BigCameraInput;
-            // TODO: save images
+
+            for (var i = 0; i < input.InputSettings.HorizontalTileCount; i++)
+            {
+                for (var j = 0; j < input.InputSettings.VerticalTileCount; j++)
+                {
+                    var sectionPath = AddPathSuffix(path, $"_{i}-{j}");
+                    var texture = RenderTextureToTexture2D(input.OutputRenderTextures[i, j]);
+                    var bytes = texture.EncodeToPNG();
+                    File.WriteAllBytes(sectionPath, bytes);
+                }
+            }
+        }
+
+        static string AddPathSuffix(string path, string suffix)
+        {
+            var directoryName = Path.GetDirectoryName(path);
+            var fileName = Path.GetFileNameWithoutExtension(path);
+            var extension = Path.GetExtension(path);
+            return Path.Combine(directoryName, fileName + suffix + extension);
+        }
+
+        static Texture2D RenderTextureToTexture2D(RenderTexture renderTexture)
+        {
+            var texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
+            RenderTexture.active = renderTexture;
+            texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            texture.Apply();
+            return texture;
         }
     }
 }
